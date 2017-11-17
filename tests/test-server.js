f504bde6dccb7363ff5ce43fa3b02931a22fedf8
Nicole Gilbert <nicolegilbert16@gmail.com>
2017-11-17 13:48:54 -0800

    added test-server file and runServer / closeServer functions in server.js

---
+++ tests/test-server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, closeServer, runServer} = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
